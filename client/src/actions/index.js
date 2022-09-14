34d780b94d6f86aebeb2e4df99adde21fbc54cbd
alen-machado <alen.achado@outlook.com>
2022-09-14 12:15:55 -0300

    primer commit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,30 +1,16 @@
-
+import axios from 'axios'
 import {GET_PRODUCTS, GET_PRODUCT_BY_NAME,GET_PRODUCT_BY_ID} from "./action-types.js"
 
 
-
-let products = [
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-      {id:"1", title:"title1", category:"paint",date:"2001",image:"http://www.marolio.com.ar/sites/default/files/styles/portfolio_3_cols/public/salsa-1.jpg?itok=Ef7rhWWe",price:"1000"},
-]
-
-
 export function getProducts(){
+      return async function(dispatch) {
+            let json = await axios.get('http://localhost:3001/artworks')
+            return dispatch ({
+                  type: GET_PRODUCTS,
+                  payload: json.data
+            })
+      }
 
-     return{
-      type:GET_PRODUCTS, payload:products
-     }
 }
 
 

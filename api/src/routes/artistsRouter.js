3e564efb64a2e1299dd1874ffb2abf7bff2f2a5c
Pedro Calvo <pedro7calvo@gmail.com>
2022-09-15 13:56:17 -0300

    cambio en artitbyId

--- api/src/routes/artistsRouter.js
+++ api/src/routes/artistsRouter.js
@@ -29,16 +29,16 @@ router.get("/:idArtist", async (req, res) => {
   try {
     const { idArtist } = req.params;
     const getArtist = await getArtists();
-    const artsName = getArtist.map((e) => JSON.stringify(e));
-    const artists = [...new Set(artsName)];
-    const artistById = artists.filter((e) => e.includes(idArtist));
-    let arr = [];
-    artistById.map((e) =>
-      arr.push({ id: e.slice(9, 31), name: e.slice(41, e.length - 2) })
-    );
+    // const artsName = getArtist.map((e) => JSON.stringify(e));
+    // const artists = [...new Set(artsName)];
+    const artistById = getArtist.filter((e) => e.id.includes(idArtist));
+    // let arr = [];
+    // artistById.map((e) =>
+    //   arr.push({ id: e.slice(9, 31), name: e.slice(41, e.length - 2) })
+    // );
     if (artistById.length === 0)
       res.status(400).json({ message: "Artist does not found" });
-    else res.status(200).send(arr);
+    else res.status(200).send(artistById);
   } catch (error) {
     console.log(error);
   }

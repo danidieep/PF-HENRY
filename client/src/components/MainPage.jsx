cab4cb000e7e6df2ca1f494e75fa6b0327016a37
Agustin Buzzalino <agustinbuzzalino94@gmail.com>
2022-09-19 21:45:05 -0300

    Css al home reestructurado

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useEffect } from "react"
-import { deleteMediumFromFilter, addFilterMedium, filterByMedium, deletePriceFromFilter, deleteArtistFromfilter, addFilterArtist, getProducts, OrderByPrice, showAllProducts, getArtists, filterByArtist, addPriceType } from "../actions"
+import { filterByMedium, deletefilter, getProducts, OrderByPrice, showAllProducts, getArtists, filterByArtist, AddFilters } from "../actions"
 import Cards from "./Cards"
 import SearchBar from "./SearchBar"
 import { useState } from "react"
@@ -31,16 +31,11 @@ export default function MainPage(props) {
   const [current, setCurrent] = useState(1)
 
 
-
-
-
   React.useEffect(() => {
     if (state.allProducts.length === 0) dispatch(getProducts())
     if (state.artistsList.length === 0) dispatch(getArtists())
     applyFilter()
   }, [state.filters])
-
-
 
 
   const handlerNext = () => {
@@ -49,6 +44,8 @@ export default function MainPage(props) {
       setNum2(num2 + ProductsPorPage)
       setCurrent(current + 1)
     }
+
+
   }
 
   const handlerPrev = () => {
@@ -66,166 +63,175 @@ export default function MainPage(props) {
   }
 
 
+  const applyFilter = () => {
+
+    state.filters.forEach(element => {
+
+      if (element.type === "artist") {
+        dispatch(filterByArtist(element.name))
+      }
 
 
+      if (element.type === "medium") {
+        dispatch(filterByMedium(element.name))
+      }
+    });
 
-
-
-  const OrderByPriceSelector = (type) => {
-    dispatch(OrderByPrice(type))
-    dispatch(addPriceType(type))
   }
-  const deletePriceFromFilter_ = (type) => {
-    dispatch(deletePriceFromFilter())
+
+  const OrderByPriceSelector = (name) => {
+    // dispatch(OrderByPrice(type))
+    dispatch(OrderByPrice(name))
+
   }
-
-
-
 
   const artistSelector = (name) => {
-    dispatch(filterByArtist(name))
-    dispatch(addFilterArtist(name))
-    handleReset()
-  }
-  const deleteArtistFromFilter_ = (name) => {
-    dispatch(deleteArtistFromfilter(name))
-  }
-
-
-  const mediumSelector = (type) => {
-    dispatch(filterByMedium(type))
-    dispatch(addFilterMedium(type))
+    dispatch(AddFilters({ type: "artist", name }))
     handleReset()
   }
 
+  const deleteFilter_ = (name) => {
+    dispatch(deletefilter(name))
+  }
 
 
-
+  const mediumSelector = (name) => {
+    dispatch(AddFilters({ type: "medium", name }))
+    // dispatch(filterByMedium(type))
+    handleReset()
+  }
 
 
   return (
-    <div>
+    <div className={styles.container}>
 
-      {state.productsFiltered.length > 0 ?
-        <div id="container" className={styles.container}>
+      {
+        //si hay productos filtras y no hay mensaje de error
+        state.productsFiltered.length > 0 && !state.notFound.message ?
+
           <div className={styles.content}>
-            <header >
-              <div className={styles.header} >
 
+            <header >
+              {/* <div className={styles.content2}> */}
+              <div className={styles.header}>
+                <div>
+                  <button className={styles.Products}>About us</button>
+                </div>
                 <div>
                   <button onClick={() => {
                     dispatch(getProducts())
                     dispatch(showAllProducts())
-                  }} className={styles.buttonsHeader}>Show all Products!</button>
+                  }} className={styles.Products}>Show all Products</button>
                 </div>
                 <div>
+                  <h1 className={styles.logo}>Artket</h1>
+                </div>
+                <div className={styles.SearchBarHome}>
                   <SearchBar handleReset={handleReset} ></SearchBar>
                 </div>
+                <button className={styles.buttonProfile}>Log in</button>
                 <div>
-                  <button>MyProfile</button>
+                  <button className={styles.buttonProfile}>Register</button>
                 </div>
-              </div>
-              <div className={styles.tapaHeader}>
+
               </div>
             </header>
-            {state.productsFiltered.length > 5 ?
-              (<div className={styles.carrusel}>
-                <div>
-                  <ul>
-                    {state.productsFiltered.slice(num1, num2).slice(0, 5).map(element => {
-                      return (
-                        <li><img src={element.image}></img></li>
+
+
+            <carrusel>
+              <p className={styles.featured}>Featured</p>
+              {state.productsFiltered.length > 5 ?
+                (<div className={styles.carrusel}>
+                  <div>
+                    <ul>
+                      {state.productsFiltered.slice(num1, num2).slice(0, 5).map(element => {
+                        return (
+                          <li><img src={element.image}></img></li>
+                        )
+                      }
+                      )
+                      }
+                    </ul>
+                  </div>
+                </div>) :
+                (<div></div>)
+              }
+            </carrusel>
+
+
+
+            {/* FILTROS */}
+            <div className={styles.filtersDiv}>
+              {/* <p className={styles.filter}>Filters</p> */}
+              <div className={styles.select}>
+                <form>
+                  <label>By price </label>
+                  <select className={styles.filters} name="" id="" onChange={(event) => OrderByPriceSelector(event.target.value)} defaultValue="base">
+                    <option disabled={true} value="base">-------</option>
+                    <option value="OrderByMoreExpensive">More expensive</option>
+                    <option value="OrderByLessExpensive">Less expensive</option>
+                  </select>
+                </form>
+              </div>
+
+              <div className={styles.select}>
+                <form>
+                  <label>By Artist </label>
+                  <select className={styles.filters} name="" id="" onChange={(event) => artistSelector(event.target.value)} defaultValue="base">
+                    <option disabled={true} value="base">-------</option>
+                    {
+                      state.artistsList?.map(element => {
+
+                        return (<option value={element.name}>{element.name}</option>)
+                      }
                       )
                     }
-                    )}
-                  </ul>
+
+                  </select>
+                </form>
+              </div>
+
+              <div className={styles.select}>
+                <form>
+                  <label>By medium </label>
+                  <select className={styles.filters} name="" id="" onChange={(event) => mediumSelector(event.target.value)} defaultValue="base">
+                    <option disabled={true} value="base">-------</option>
+                    {
+                      state.mediums.map(element => {
+
+                        return (<option value={element}>{element}</option>)
+                      }
+                      )
+                    }
+
+                  </select>
+                </form>
+              </div>
+            </div>
+
+
+            {/* LIMPIAR FILTROS */}
+            {state.filters.map(element => {
+              return (
+                <div>
+                  <button onClick={() => deleteFilter_(element)}>X</button>
+                  <span>{element.name}</span>
                 </div>
-              </div>) :
-              (<div></div>)
+              )
+            }
+            )
             }
 
-
-            <div className={styles.body}>
-
-              <div className={styles.filter_box}>
-                <div className={styles.filter_box_2} >
-                  <form>
-                    <label>By price</label>
-                    <select className={styles.filters} name="" id="" onChange={(event) => OrderByPriceSelector(event.target.value)} defaultValue="base">
-                      <option disabled={true} value="base">-------</option>
-                      <option value="OrderByMoreExpensive">More expensive</option>
-                      <option value="OrderByLessExpensive">Less expensive</option>
-                    </select>
-                  </form>
-                  <form>
-                    <label>By Artist</label>
-                    <select className={styles.filters} name="" id="" onChange={(event) => artistSelector(event.target.value)} defaultValue="base">
-                      <option disabled={true} value="base">-------</option>
-                      {
-                        state.artistsList.map(element => {
-
-                          return (<option value={element.name}>{element.name}</option>)
-                        }
-                        )
-                      }
-
-                    </select>
-                  </form>
-                  <form>
-                    <label>By medium</label>
-                    <select className={styles.filters} name="" id="" onChange={(event) => mediumSelector(event.target.value)} defaultValue="base">
-                      <option disabled={true} value="base">-------</option>
-                      {
-                        state.mediums.map(element => {
-
-                          return (<option value={element}>{element}</option>)
-                        }
-                        )
-                      }
-
-                    </select>
-                  </form>
-                </div>
-              </div>
-
-              {state.filterArtist.map(element => {
-                return (
-                  <div>
-                    <button onClick={() => deleteArtistFromFilter_(element)}>X</button>
-                    <span>{element}</span>
-                  </div>
-                )
-              })}
-              {
-                state.orderByPrice.map(element => {
-                  return (
-                    <div>
-                      <button onClick={() => deletePriceFromFilter_(element)} >X</button>
-                      <span>{element}</span>
-                    </div>
-                  )
-                })
-              }
-              {state.filterMediums.map(element => {
-                return (
-                  <div>
-                    <button onClick={() => deleteMediumFromFilter_(element)}>X</button>
-                    <span>{element}</span>
-                  </div>
-                )
-              })}
-              <div className={styles.cards}>
-                {state.productsFiltered.slice(num1, num2).map(element => (
-                  <div id="card" ><Cards data={element} key={element.id} /></div>
-                )
-                )}
-              </div>
-
-
-
-
-
+            {/* CARDS  */}
+            <p className={styles.featured}>Galery</p>
+            <div className={styles.cards}>
+              {state.productsFiltered.slice(num1, num2).map(element => (
+                <div id="card" ><Cards data={element} key={element.id} /></div>
+              )
+              )}
             </div>
+
+
             <footer className={styles}>
               <div>
                 <button onClick={handlerPrev} className={styles.buttonsNavegation}><p className={styles.textInButtonNavegation}>{`<`}</p></button>
@@ -239,11 +245,8 @@ export default function MainPage(props) {
                         setCurrent(i + 1)
                       }
                     } className={styles.buttonsNavigation} style={i + 1 === current ? { fontSize: "1.3rem" } : { color: "black" }}>{i + 1}</button>
-
                   )
                   )
-
-
                 }
                 <button onClick={handlerNext} className={styles.buttonsNavegation}> <p className={styles.textInButtonNavegation}>{`>`}</p></button>
               </div>
@@ -254,100 +257,9 @@ export default function MainPage(props) {
 
               </div>
             </footer>
-          </div>:
-
-
-        </div>
-        :
-        <div>
-          <header >
-            <div className={styles.header} >
-
-              <div>
-                <button onClick={() => {
-                  dispatch(getProducts())
-                  dispatch(showAllProducts())
-                }} className={styles.buttonsHeader}>Show all Products!</button>
-              </div>
-              <div>
-                <SearchBar handleReset={handleReset} ></SearchBar>
-              </div>
-              <div>
-                <button>MyProfile</button>
-              </div>
-            </div>
-            <div className={styles.tapaHeader}>
-            </div>
-          </header>Loading...</div>
-      }
+          </div> : <div><p>No anda esto</p> </div>}
     </div>
   )
 }
-//             </header>
-//             <div className={styles.filter_box}>
-//               <div className={styles.filter_box_2} >
-//                 <form>
-//                     <label>By price</label>
-//                     <select  className={styles.filters} name="" id=""  onChange={(event)=>OrderByPriceSelector(event.target.value)} defaultValue="base">
-//                       <option disabled={true} value="base">-------</option>
-//                       <option  value="OrderByMoreExpensive">More expensive</option>
-//                       <option  value="OrderByLessExpensive">Less expensive</option>
-//                     </select>
-//                   </form>
-//                   <form>
-//                     <label>By Artist</label>
-//                     <select  className={styles.filters} name="" id=""  onChange={(event)=>artistSelector(event.target.value)} defaultValue="base">
-//                       <option disabled={true} value="base">-------</option>
-//                       {
-//                       state.artistsList.map(element => {
-
-//                         return( <option value={element.name}>{element.name}</option>)
-//                       }
-//                     )
-//                   }
-
-//                     </select>
-//                   </form>
-//                   <form>
-//                     <label>By medium</label>
-//                     <select  className={styles.filters} name="" id=""  onChange={(event)=>mediumSelector(event.target.value)} defaultValue="base">
-//                       <option disabled={true} value="base">-------</option>
-//                       {
-//                       state.mediums.map(element => {
-
-//                         return( <option value={element}>{element}</option>)
-//                       }
-//                     )
-//                   }
-
-//                     </select>
-//                   </form>
-//                   {state.filters.map(element => {
-//                     return (
-//                       <div>
-//                       <button onClick={()=> deleteFilter_(element)}>X</button>
-//                       <span>{element.name}</span>
-//                       </div>
-//                     )
-//                   })}
-
-//                   </div>
-//                </div>
-
-//             <h1>sorry we dont have artworks with that filters</h1>
-//             </div>
-//             :
-//             <div></div>
-//             }
-//         </div>
-//     )
-// >>>>>>> 8e15a322f9c098ff71f07f124dac0ae06c037ebd
-
-
-
-
-
-
-
 
 

c389b1b8c9ed48b82de42d755166f6911a08f2be
Pedr0calvo <100429639+Pedr0calvo@users.noreply.github.com>
2022-09-28 18:17:28 -0300

    Update middlewares.js

--- client/src/middlewares/middlewares.js
+++ client/src/middlewares/middlewares.js
@@ -22,29 +22,18 @@
 //      }
 // }
 
-export const addProductToCarrito = (payload) =>{
+export const addProductToCarrito = (payload) => {
+  fetch(`/cart/${payload.artId}`, {
+    method: "POST",
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+    body: JSON.stringify(payload.email),
+  });
+};
 
-    fetch(`/cart/${payload.artId}`,{
-          method: "POST",
-          headers:{"Accept": "application/json",
-         "Content-Type":"application/json"},            
-       body:JSON.stringify(payload.email)
-          }
-      )
-
-
-}
-
-
-export const deleteProductFromCarrito = (payload)=>{
-
-    fetch(`/cart/${payload.artId}`,{
-          method: "POST",
-          headers:{"Accept": "application/json",
-         "Content-Type":"application/json"},            
-       body:JSON.stringify(payload.email)
-          }
-      )
-
-
-}
+export const deleteProductFromCarrito = (payload) => {
+  fetch(`/cart/${payload.artId}`, {
+    method: "POST",
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+    body: JSON.stringify(payload.email),
+  });
+};

bfe752fbf0e8ef8453b966a7d92c2fdc33af62c9
Pedr0calvo <100429639+Pedr0calvo@users.noreply.github.com>
2022-10-03 20:08:20 -0300

    Update adressRouter.js

--- api/src/routes/adressRouter.js
+++ api/src/routes/adressRouter.js
@@ -3,42 +3,33 @@ const { Adress } = require("../db");
 
 router.get("/", async (req, res) => {
   const { email } = req.headers;
-  console.log(email, "email req.headers");
   const adresses = await Adress.findOne({ where: { email } });
   res.status(200).send(adresses);
 });
 
 router.post("/", async (req, res) => {
   const { payload, email } = req.body;
-  console.log(req.body, "req.body");
-  Adress.findOrCreate({
-    where: { email },
-    defaults: {
-      street: payload.street,
-      number: payload.number,
-      postalCode: payload.postalCode,
-    },
+  Adress.create({
+    email,
+    street: payload.street,
+    number: payload.number,
+    postalCode: payload.postalCode,
   });
   res.status(200).send("Adress");
 });
 
 router.put("/", async (req, res) => {
   const { payload, email } = req.body;
-  if (
-    payload.street.length ||
-    payload.number.length ||
-    payload.postalCode.length
-  ) {
-    Adress.update(
-      {
-        street: payload.street,
-        number: payload.number,
-        postalCode: payload.postalCode,
-      },
-      { where: { email } }
-    );
-    res.status(200).send("Adress");
-  } else return;
+  if (payload.street.length) {
+    Adress.update({ street: payload.street }, { where: { email } });
+  }
+  if (payload.number.length) {
+    Adress.update({ number: payload.number }, { where: { email } });
+  }
+  if (payload.postalCode.length) {
+    Adress.update({ postalCode: payload.postalCode }, { where: { email } });
+  }
+  res.status(200).send("Adress");
 });
 
 module.exports = router;

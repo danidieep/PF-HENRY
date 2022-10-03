9a71842a0613b7acef75d76e51414b7a99ef9a8c
Agustin Buzzalino <agustinbuzzalino94@gmail.com>
2022-10-03 03:23:56 -0300

    carrito y favs hechos con muchisimo amor

--- client/src/components/Favoritos.jsx
+++ client/src/components/Favoritos.jsx
@@ -5,6 +5,9 @@ import { useEffect } from "react";
 import styles from './ModulesCss/Carrito.module.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import LogOut from "./LogOut";
+import { BsFillHeartFill } from "react-icons/bs"
+import { BsFillCartFill } from "react-icons/bs"
 
 
 
@@ -13,7 +16,7 @@ export default function Favoritos() {
   const user = JSON.parse(localStorage.getItem("user"))
   const favoritos = useSelector((state) => state.favoritos)
   const dispatch = useDispatch();
-
+  const carrito = useSelector((state) => state.carrito)
   const email = JSON.parse(localStorage.getItem("user"))[0].email
 
 
@@ -44,13 +47,74 @@ export default function Favoritos() {
 
   return (
     <div className={styles.containerCarrito}>
+      <header >
+        <div className={styles.tapaHeader}></div>
+        <div className={styles.header}>
+          <div className={styles.filtersDiv}>
+            <Link className={styles.link} to='/mainpage'>
+              <button className={styles.logoDetails}
+              ><h2 className={styles.logo}>Artket</h2></button>
+            </Link>
+            <div></div>
+            <div><h1 className={styles.divTittle}>Favorites</h1></div>
+            <div></div>
+            <div className={styles.restoDeItems}>
+              <div className={styles.cartAndProfileAndFav} >
+                {JSON.parse(localStorage.getItem("user")).length ?
+                  <div className={styles.CartAndFav}>
 
+
+                    <div className={styles.iconsHeader}>
+                      <Link to="/ShopCart">
+
+                        <button className={styles.btnCarritoNav}>
+                          <BsFillCartFill />
+                          <h4 className={styles.cantItems}>{carrito.length}</h4>
+                        </button>
+
+
+
+                      </Link>
+                    </div>
+                    <div className={styles.iconsHeader}>
+                      <Link to="/Favourites">
+                        <button className={styles.btnFav}>
+                          <BsFillHeartFill />
+                          <h4 className={styles.cantItems}>{favoritos.length}</h4>
+                        </button>
+
+                      </Link>
+                    </div>
+                    <div className={styles.profileBtn}>
+                      <LogOut></LogOut>
+                    </div>
+                  </div>
+                  : false
+                }
+
+
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </header>
       <div className={styles.yourCarrito}>
         <ToastContainer />
-        <header>
-          <h1>Favorites</h1>
-        </header>
+
+
       </div>
+      {
+        favoritos.length === 0 ?
+          <div className={styles.favEmpty}>
+            <div>
+              <p>You have nothing on your favorites list</p>
+              <p>Don't know what to buy? Lots of arkwort are waiting for you!</p>
+            </div>
+          </div>
+          : false
+      }
 
       {favoritos.map((element) => {
         return (
@@ -61,7 +125,7 @@ export default function Favoritos() {
               <h1 className={styles.priceCarrito}> ${element.price}</h1>
               <div className={styles.btnCarritoPos}>
                 <Link to={`/Products/${element.id}`}>
-                  <button className={styles.btnCarrito}>View artwork</button>
+                  <button className={styles.btnCarrito}>Check artwork</button>
                 </Link>
                 <button className={styles.btnCarrito} onClick={() => eliminar(element.id)}>Delete</button>
               </div>
@@ -72,9 +136,9 @@ export default function Favoritos() {
         );
       })}
       <div className={styles.btnHomePos}>
-        <Link to='/MainPage'>
+        {/* <Link to='/MainPage'>
           <button className={styles.btnHome}>Home</button>
-        </Link>
+        </Link> */}
       </div>
 
     </div>
--- client/src/components/ShopCart.jsx
+++ client/src/components/ShopCart.jsx
@@ -7,6 +7,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 import styles from './ModulesCss/Carrito.module.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import LogOut from "./LogOut";
+import { BsFillHeartFill } from "react-icons/bs"
+import { BsFillCartFill } from "react-icons/bs"
 
 export default function ShopCart() {
 
@@ -15,6 +18,7 @@ export default function ShopCart() {
   const dispatch = useDispatch();
   const product = useSelector((state) => state.productDetails);
   const email = JSON.parse(localStorage.getItem("user"))[0].email
+  const favoritos = useSelector((state) => state.favoritos)
 
 
   const eliminar = (id) => {
@@ -67,18 +71,82 @@ export default function ShopCart() {
   }
   return (
     <div className={styles.containerCarrito}>
+      <header >
+        <div className={styles.tapaHeader}></div>
+        <div className={styles.header}>
+          <div className={styles.filtersDiv}>
+            <Link className={styles.link} to='/mainpage'>
+              <button className={styles.logoDetails}
+              ><h2 className={styles.logo}>Artket</h2></button>
+            </Link>
+            <div></div>
+            <div><h1 className={styles.divTittle}>Your Cart</h1></div>
+            <div></div>
+            <div className={styles.restoDeItems}>
+              <div className={styles.cartAndProfileAndFav} >
+                {JSON.parse(localStorage.getItem("user")).length ?
+                  <div className={styles.CartAndFav}>
 
+
+                    <div className={styles.iconsHeader}>
+                      <Link to="/ShopCart">
+
+                        <button className={styles.btnCarritoNav}>
+                          <BsFillCartFill />
+                          <h4 className={styles.cantItems}>{carrito.length}</h4>
+                        </button>
+
+
+
+                      </Link>
+                    </div>
+                    <div className={styles.iconsHeader}>
+                      <Link to="/Favourites">
+                        <button className={styles.btnFav}>
+                          <BsFillHeartFill />
+                          <h4 className={styles.cantItems}>{favoritos.length}</h4>
+                        </button>
+
+                      </Link>
+                    </div>
+                    <div className={styles.profileBtn}>
+                      <LogOut></LogOut>
+                    </div>
+                  </div>
+                  : false
+                }
+
+
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </header>
       <div className={styles.yourCarrito}>
         <ToastContainer />
-        <h1>Your Cart </h1>
+
       </div>
-      
+      {
+        carrito.length === 0 ?
+          <div className={styles.favEmpty}>
+            <div>
+              <p>You have nothing on your cart</p>
+              <p>Don't know what to buy? Lots of arkwort are waiting for you!</p>
+            </div>
+          </div>
+          : false
+      }
       {carrito.map((element) => {
         return (
           <div className={styles.allCarritoContainer}>
             <div className={styles.carrito}>
-              <img className={styles.imgCarrito} src={element.image} alt="" />
+              <Link to={'/Products/' + element.id}>
+                <img className={styles.imgCarrito} src={element.image} alt="" />
+              </Link>
               <h1 className={styles.titleCarrito}>{element.title}</h1>
+              <h5 className={styles.titleBy}>by {element.creator}</h5>
               <h1 className={styles.priceCarrito}> ${element.price}</h1>
               <div className={styles.btnCarritoPos}>
                 <button className={styles.btnCarrito} onClick={() => comprar(element.id)}>Buy Now!</button>
@@ -90,25 +158,12 @@ export default function ShopCart() {
 
         );
       })}
-      <div className={styles.btnHomePos}>
+      {/* <div className={styles.btnHomePos}>
         <Link to='/MainPage'>
           <button className={styles.btnHome}>Home</button>
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 }
 
-// useMemo(()=>{
-
-//     if(state.carrito.length){
-//       localStorage.setItem("cart",JSON.stringify(state.carrito))
-//     }
-//     if(state.carrito.length===0){
-
-//       if( JSON.parse(localStorage.getItem("cart")===null)){ localStorage.setItem("cart",JSON.stringify([]))}
-//       if( JSON.parse(localStorage.getItem("cart").length)){ state.carrito = JSON.parse(localStorage.getItem("cart")) }
-
-//     }
-
-// },[state.carrito])
